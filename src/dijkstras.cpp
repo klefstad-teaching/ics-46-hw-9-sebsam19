53c4a959297c38b89e1abbdf30ff656805ac25c1
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-10 19:33:54 +0000

    changed dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,33 +6,43 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     previous.resize(n, -1);
     vector<int> visited(n, false);
 
-    priority_queue<Edge> pq;
-    pq.push(Edge(source, 0, 0));
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, source});
     distance[source] = 0;
 
     while(!pq.empty()) {
-        Edge current = pq.top();
+        int u = pq.top().second;
         pq.pop();
-        int u = current.src;
         if (visited[u]) {
             continue;
         }
         visited[u] = true;
 
-        for (const auto& e: G[u]) {
-            int v = e.src;
+        for (Edge e: G[u]) {
+            int v = e.dst;
             int w = e.weight;
 
             if (!visited[v] && (distance[u] + w) < distance[v]) {
                 distance[v] = distance[u] + w;
                 previous[v] = u;
-                pq.push(Edge(v,distance[v], 0));
+                pq.push({distance[v], v});
             }
         }
     }
-    return {};
+    return distance;
 }
 
-// vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {}
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+    vector<int> holder;
+    for (int i = destination; i != -1; i = previous[i]) {
+        holder.insert(holder.begin(), i);
+    }
+    return holder;
+}
 
-// void print_path(const vector<int>& v, int total) {}
+void print_path(const vector<int>& v, int total) {
+    for (int i = 0; i < total; ++i) {
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
