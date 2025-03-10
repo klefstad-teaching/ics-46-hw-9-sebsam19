945af566a935919de8514d279eb3a88f7c3f441d
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-10 05:49:00 +0000

    completed generate word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -5,14 +5,34 @@ void error(string word1, string word2, string msg) {
     cout << msg << word1 << word2;
 }
 
-// vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-//     queue<vector<string>> ladder_queue;
-//     ladder_queue.push(begin_word);
-//     std::set<string> visited;
-//     visited.insert(begin_word);
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    std::set<string> visited;
+    visited.insert(begin_word);
 
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
 
-// }
+        for (string words : word_list) {
+            if (is_adjacent(last_word, words)) {
+                if (visited.find(words) != visited.end()) {
+                    visited.insert(words);
+                    vector<string> new_ladder(ladder);
+                    new_ladder.push_back(words);
+                    if (words == end_word) {
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
+
+}
 
 bool is_adjacent(const string& word1, const string& word2) {
     int diff = 0;
@@ -66,3 +86,7 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     }
     return choices[str1.size()][str2.size()] <= d;
 }
+
+// void load_words(set<string> & word_list, const string& file_name);
+// void print_word_ladder(const vector<string>& ladder);
+// void verify_word_ladder();
