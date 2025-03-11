3ae63a69f59abbdf9cd8659f85a2e6ad971a0cb6
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-11 06:39:51 +0000

    fixing printing formatting

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -50,7 +50,7 @@ void print_path(const vector<int>& v, int total) {
         for (size_t i = 0; i <v.size(); ++i) {
             cout << v[i] << " ";
         }
+        cout << endl;
     }
-    cout << endl;
     cout << "Total cost is " << total << endl;
 }
