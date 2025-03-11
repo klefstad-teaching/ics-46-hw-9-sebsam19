8612726e0fe927951e1b1dc8ad9f177929659e04
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-11 06:58:25 +0000

    removed inf check since the error is gone

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -35,9 +35,6 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> holder;
 
-    if (distances[destination] == INF) {
-        return holder;
-    }
     for (int i = destination; i != -1; i = previous[i]) {
         holder.push_back(i);
     }
