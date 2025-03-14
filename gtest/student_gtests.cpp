4fb1c7ac4e77fcb2f698f1bd0ac5bf2a03758bee
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-03-14 03:31:34 +0000

    final edits and cleaned up

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,22 @@
 #include "../src/dijkstras.h"
 #include "../src/ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(true);
+TEST(dijkstras_insertion, inserting) {
+  Graph G;
+  file_to_graph("src/small.txt", G);
+  vector<int> p(G.numVertices, -1);
+  int src = 0;
+  int dest = 3;
+
+  vector<int> distance = dijkstra_shortest_path(G, src, p);
+  vector<int> path = extract_shortest_path(distance, p, dest);
+
+  EXPECT_TRUE(path[0] == 0);
+  EXPECT_TRUE(path[1] == 3);
+}
+
+TEST(ladder_testing, ladder) {
+  set<string> word_list;
+  load_words(word_list, "src/words.txt");
+  EXPECT_TRUE(generate_word_ladder("cat", "dog", word_list).size() == 4);
 }
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char * argv[]) {
     file_to_graph("src/small.txt", G);
     vector<int> p(G.numVertices, -1);
     int src = 0;
-    int dest = 12;
+    int dest = 2;
 
     vector<int> distances = dijkstra_shortest_path(G, src, p);
     vector<int> path = extract_shortest_path(distances, p, dest);
 
-    cout << "shortest path" << " : ";
+    cout << "shortest path" << " : ";    
     print_path(path, path.size());
     return result;
 }
